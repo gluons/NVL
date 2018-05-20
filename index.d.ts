ff4ec1e2d3992483b2d6a51dc284c2cf29dbec3b
Saran Tanpituckpong <sarunta@gmail.com>
2018-05-20 18:26:18 +0700

    Update TypeScript declaration

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,16 @@
 /**
  * Return a fallback value from `replaceWith` when a main `value` is blank (`undefined` or `null`).
  *
+ * @template T Type.
+ * @param {T} value Main value.
+ * @param {T} replaceWith Fallback value.
+ * @returns {T}
+ */
+declare function nvl<T>(value: T, replaceWith: T): T;
+
+/**
+ * Return a fallback value from `replaceWith` when a main `value` is blank (`undefined` or `null`).
+ *
  * @param {*} value Main value.
  * @param {*} replaceWith Fallback value.
  * @returns {*}
