f198011b5d80a4236c3a9681b59f5607cd674b3a
Saran Tanpituckpong <sarunta@gmail.com>
2019-02-06 00:22:26 +0700

    :building_construction: Change testing to Jest

---
+++ test/nvl.test.ts
@@ -0,0 +1,26 @@
+import nvl from '../dist';
+
+describe('NVL', () => {
+	it('shoud return `value` when `value` is not blank value', () => {
+		const now = new Date();
+		const emptyArr = [];
+		const emptyObj = {};
+
+		expect(nvl(now, new Date(0))).toBe(now);
+		expect(nvl(emptyArr, [1])).toBe(emptyArr);
+		expect(nvl(emptyObj, { a: 1, b: 2 })).toBe(emptyObj);
+		expect(nvl(false, true)).toStrictEqual(false);
+	});
+	it('shoud return `fallbackValue` when `value` is undefined.', () => {
+		const notYet = void 0;
+		const now = new Date();
+
+		expect(nvl(notYet, now)).toBe(now);
+	});
+	it('shoud return `fallbackValue` when `value` is null.', () => {
+		const nil = null;
+		const now = new Date();
+
+		expect(nvl(nil, now)).toBe(now);
+	});
+});
